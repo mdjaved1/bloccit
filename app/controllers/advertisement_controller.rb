50bab9273549a322bcde3fdfb1f9ffb15394af80
Muhammad <daudjaved1@gmail.com>
2017-07-02 05:09:37 +0000

    something wrong wiht c9

--- app/controllers/advertisement_controller.rb
+++ app/controllers/advertisement_controller.rb
@@ -4,7 +4,6 @@ class AdvertisementController < ApplicationController
   end
 
   def show
-    
   end
 
   def new
--- spec/controllers/advertisement_controller_spec.rb
+++ spec/controllers/advertisement_controller_spec.rb
@@ -1,33 +1,26 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe AdvertisementController, type: :controller do
-
+  let (:my_ad) do
+    Advertisement.create(
+      id: 1,
+      title: RandomData.random_sentence,
+      body: RandomData.random_paragraph,
+      price: 99
+    )
+  end
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
-  end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    
+    it "assigns [my_ad] to @advertisements" do
+      get :index
+      expect(assigns(:advertisement)).to eq([my_ad])
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
 
 end
