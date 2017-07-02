6ea109ec97db5ab465d3794875a798730bfcabda
Muhammad <daudjaved1@gmail.com>
2017-07-02 04:39:20 +0000

    commiting fix

---
+++ app/controllers/advertisement_controller.rb
@@ -0,0 +1,15 @@
+class AdvertisementController < ApplicationController
+  def index
+    @advertisement = Advertisement.all
+  end
+
+  def show
+    
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
