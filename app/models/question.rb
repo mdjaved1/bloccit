a84a7f4280b740144c512ce31a608b9c6ff6fd96
Muhammad <daudjaved1@gmail.com>
2017-07-03 15:06:28 +0000

    commit fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,16 +1,3 @@
 class Question < ActiveRecord::Base
-    def index
-      @questions  = Question.all
-    end
-    
-    def show
-        
-    end
-    
-    def new
-    end
-    
-    def edit
-        
-    end
+
 end
